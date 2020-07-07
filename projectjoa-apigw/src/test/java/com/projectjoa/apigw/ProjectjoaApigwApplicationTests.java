265aef02deff5bdfdb5f0300c6772bc6b0f3df75
ChoChulhyun <irc2121@gmail.com>
2020-07-07 21:53:51 +0900

    projectjoa-apigw registered

---
+++ projectjoa-apigw/src/test/java/com/projectjoa/apigw/ProjectjoaApigwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectjoa.apigw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectjoaApigwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
