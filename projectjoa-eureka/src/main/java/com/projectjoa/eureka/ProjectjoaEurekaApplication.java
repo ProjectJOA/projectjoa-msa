a8672c9bba2016feac0930576230cca8956d0453
ChoChulhyun <irc2121@gmail.com>
2020-07-07 21:49:35 +0900

    projectjoa-eureka registered

---
+++ projectjoa-eureka/src/main/java/com/projectjoa/eureka/ProjectjoaEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.projectjoa.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ProjectjoaEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectjoaEurekaApplication.class, args);
+	}
+
+}
---
+++ projectjoa-eureka/src/test/java/com/projectjoa/eureka/ProjectjoaEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectjoa.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectjoaEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
