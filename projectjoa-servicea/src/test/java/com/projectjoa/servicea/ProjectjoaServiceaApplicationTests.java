8a790432e1b90b6fb2ce802237ac5c327751df8f
ChoChulhyun <irc2121@gmail.com>
2020-07-07 21:57:04 +0900

    projectjoa-servicea registered

---
+++ projectjoa-servicea/src/test/java/com/projectjoa/servicea/ProjectjoaServiceaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectjoa.servicea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectjoaServiceaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
